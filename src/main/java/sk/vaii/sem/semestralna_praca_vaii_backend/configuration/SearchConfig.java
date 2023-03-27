518c05489efd546db8f46571077604c0317efe1b
DavidZimen <david.zimen1@gmail.com>
2023-03-27 18:54:40 +0200

    search extension version 1.0.1

--- src/main/java/sk/vaii/sem/semestralna_praca_vaii_backend/configuration/SearchConfig.java
+++ src/main/java/sk/vaii/sem/semestralna_praca_vaii_backend/configuration/SearchConfig.java
@@ -1,27 +1,29 @@
 package sk.vaii.sem.semestralna_praca_vaii_backend.configuration;
 
+import com.scheidtbachmann.ps.search.searchextension.config.adapter.SearchExtensionConfigAdapter;
 import com.scheidtbachmann.ps.search.searchextension.configuration.ApiPropertiesConfig;
 import com.scheidtbachmann.ps.search.searchextension.dto.SearchResult;
-import com.scheidtbachmann.ps.search.searchextension.service.SearchService;
-import com.scheidtbachmann.ps.search.searchextension.service.Searchable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
 
 @Configuration
-public class SearchConfig {
+public class SearchConfig extends SearchExtensionConfigAdapter<SearchResult> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SearchConfig.class);
 
     @Value("${apiconfig.api-version}")
     private String apiVersion;
 
-    @Bean
+    @Override
+    protected Logger getLogger() {
+        return LOGGER;
+    }
+
+    @Override
     @ConfigurationProperties(prefix = "apiconfig")
     public ApiPropertiesConfig apiPropertiesConfig() {
         ApiPropertiesConfig apiPropertiesConfig = new ApiPropertiesConfig();
@@ -32,10 +34,5 @@ public class SearchConfig {
 
         return apiPropertiesConfig;
     }
-
-    @Bean
-    public SearchService<SearchResult> searchService(List<Searchable<SearchResult>> searchables) {
-        LOGGER.info("Creating Bean of {}", SearchService.class.getName());
-        return new SearchService<>(searchables);
-    }
 }
+
